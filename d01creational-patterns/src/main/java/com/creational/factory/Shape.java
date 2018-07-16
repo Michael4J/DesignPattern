f002b61d87ae53797eaafc815e0a78ac6cf824b1
Michael4J <874382676@qq.com>
2018-07-16 17:48:26 +0800

    DesignPattern created 设计模式

---
+++ d01creational-patterns/src/main/java/com/creational/factory/Shape.java
@@ -0,0 +1,14 @@
+package com.creational.factory;
+
+/**
+ * @Title: Shape
+ * @Auther: MichaelJ
+ * @Date: 2018-7-16
+ * @Description: 形状接口
+ */
+public interface Shape {
+    /**
+     * 行为:画
+     */
+    void draw();
+}
